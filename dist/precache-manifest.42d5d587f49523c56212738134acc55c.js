671072c617113b8191d20242a7ee70af16c3266f
denis_shkotov <denis_shkotov@skbkontur.ru>
2018-12-06 19:06:43 +0500

    example

---
+++ dist/precache-manifest.42d5d587f49523c56212738134acc55c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f1f6247782e12220bbb7601e3c21e33",
+    "url": "/static/media/Highlight.9f1f6247.less"
+  },
+  {
+    "revision": "3ac6ab30427d1b0f6845",
+    "url": "/static/css/main.e1cbe877.chunk.css"
+  },
+  {
+    "revision": "1e5ce5123257455030512d16cc5b2f81",
+    "url": "/static/media/Tooltip.1e5ce512.less"
+  },
+  {
+    "revision": "c9323a7eb853bfe418ea",
+    "url": "/static/js/1.c9323a7e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3ac6ab30427d1b0f6845",
+    "url": "/static/js/main.3ac6ab30.chunk.js"
+  },
+  {
+    "revision": "3dfa14e71f7ce5b112497e368d75e263",
+    "url": "/static/media/Points.3dfa14e7.less"
+  },
+  {
+    "revision": "4e5d23e02e5a259183f6509b9eda3ef2",
+    "url": "/static/media/TourButton.4e5d23e0.less"
+  },
+  {
+    "revision": "6bc760a7016b050194583a38b70dcdc6",
+    "url": "/static/media/Footer.6bc760a7.less"
+  },
+  {
+    "revision": "c9323a7eb853bfe418ea",
+    "url": "/static/css/1.bbfabbb3.chunk.css"
+  },
+  {
+    "revision": "d6ccf62323dcbff6c6f5ad6214fef482",
+    "url": "/index.html"
+  }
+];
